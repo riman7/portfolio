9aa0ea2b939a7e0bad0e998a84a12c3d24d2c483
riman7 <r.i.m.a.n.maharjan7@gmail.com>
2022-10-14 12:15:58 +0545

    Update darkMode.js

--- darkMode.js
+++ darkMode.js
@@ -1,10 +1,20 @@
 if(localStorage.getItem("mode") == "dark-theme"){
     document.body.classList.add("dark-theme");
-    document.getElementById("dark-theme-icon").src = "./photos/sun.png";
+    if(document == "https://riman7.github.io/portfolio/"){
+        document.getElementById("dark-theme-icon").src = "./photos/sun.png";
+    }
+    else{
+        document.getElementById("dark-theme-icon").src = "../photos/sun.png";
+    }
 }
 if(localStorage.getItem("mode") == "bright-theme"){
     document.body.classList.remove("dark-theme");
-    document.getElementById("dark-theme-icon").src = "./photos/moon.png";
+    if(document == "https://riman7.github.io/portfolio/"){
+        document.getElementById("dark-theme-icon").src = "./photos/moon.png";
+    }
+    else{
+        document.getElementById("dark-theme-icon").src = "../photos/moon.png";
+    }
 }
 console.log(document);
 darkMode = () => {
@@ -16,11 +26,21 @@ darkMode = () => {
     }
     if(localStorage.getItem("mode") == "dark-theme"){
         document.body.classList.add("dark-theme");
+        if(document == "https://riman7.github.io/portfolio/"){
         document.getElementById("dark-theme-icon").src = "./photos/sun.png";
+    }
+    else{
+        document.getElementById("dark-theme-icon").src = "../photos/sun.png";
+    }
     }
     if(localStorage.getItem("mode") == "bright-theme"){
         document.body.classList.remove("dark-theme");
+        if(document == "https://riman7.github.io/portfolio/"){
         document.getElementById("dark-theme-icon").src = "./photos/moon.png";
+        }
+        else{
+            document.getElementById("dark-theme-icon").src = "../photos/moon.png";
+        }
     }
    
 }
